acfe8b0b399c853fd14d89a0d9096e2499580d60
Jared <Idarkcrackulous@gmail.com>
2019-02-06 02:02:03 -0500

    Starting on Bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+	title = models.CharField(max_length=250)
+	pub_date = models.DateTimeField()
+	body = models.TextField(max_length=1500)
+	image = models.ImageField(upload_to='images/')
+
+
+### For future projects 
+
+# Create the model above for application dependant on whats needed
+
+# Add the Application to settings check settings.py for reference
+
+# Add the application to admin with admin.sites.register(appname)
+
+# Create a migration with python manage.py makemigration
+
+# Migrate the application with python manage.py migrate
--- jobs/models.py
+++ jobs/models.py
@@ -3,3 +3,15 @@ from django.db import models
 class Job(models.Model):
 	image = models.ImageField(upload_to='images/')
 	summary = models.CharField(max_length=200)
+
+### For future projects 
+
+# Create the model above for application dependant on whats needed
+
+# Add the Application to settings check settings.py for reference
+
+# Add the application to admin with admin.sites.register(appname)
+
+# Create a migration with python manage.py makemigration
+
+# Migrate the application with python manage.py migrate
